7ce920fcbb7b7dae9341080c832932c216c2820d
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-09-04 12:43:10 +0100

    OSM (#1)
    fix/osm integration
    
    authored-by: jarbasal <jarbasai@mailfence.com>

--- __init__.py
+++ __init__.py
@@ -1,14 +1,11 @@
 import datetime
 import time
+
 import validators
 from mycroft.skills.core import (MycroftSkill,
-                                 intent_handler, intent_file_handler)
-from mycroft.messagebus.message import Message
+                                 intent_file_handler)
+from ovos_skills_manager.github.utils import author_repo_from_github_url
 from ovos_skills_manager.osm import OVOSSkillsManager
-from ovos_skills_manager.appstores.pling import get_pling_skills
-from ovos_skills_manager.appstores.ovos import get_ovos_skills
-from json_database import JsonStorage
-from os.path import join, dirname, abspath
 
 
 class OSMInstaller(MycroftSkill):
@@ -18,15 +15,12 @@ class OSMInstaller(MycroftSkill):
         self.osm_manager = OVOSSkillsManager()
         self.osm_manager.enable_appstore("ovos")
         self.osm_manager.enable_appstore("pling")
-        self.ovos_skills_model = []
-        self.pling_skills_model = []
-        self.search_skills_model = []
-        self.ovosDB = join(self.file_system.path, 'ovos-list.db')
-        self.ovos_storage = JsonStorage(self.ovosDB)
-        self.plingDB = join(self.file_system.path, 'pling-list.db')
-        self.pling_storage = JsonStorage(self.plingDB)
+        self.default_icon = "https://iconarchive.com/download/i103156\
+                        /blackvariant/button-ui-requests-9/Parcel.ico"
 
     def initialize(self):
+        self.osm_manager.bind(self.bus)
+
         self.add_event("OSMInstaller.openvoiceos.home",
                        self.handle_display_home)
         self.add_event("osm.sync.finish",
@@ -36,129 +30,90 @@ class OSMInstaller(MycroftSkill):
         self.gui.register_handler("OSMInstaller.openvoiceos.install",
                                   self.handle_install)
 
-        # Build The Initial Display Model Without Sync
-        # Make Sure Models Are Built Early To Avoid Display Call Delay
-        self.update_display_model()
-
-        # First Sync
-        s = self.osm_manager.get_active_appstores()
-        self.log.info(s.keys())
-        self.osm_manager.sync_appstores()
         # Start A Scheduled Event for Syncing OSM data
         now = datetime.datetime.now()
-        callback_time = datetime.datetime(
-            now.year, now.month, now.day, now.hour, now.minute
-        ) + datetime.timedelta(seconds=60)
-        self.schedule_repeating_event(self.sync_osm_model, callback_time, 9000)
+        self.schedule_repeating_event(self.sync_osm_model, now, 9000)
 
     @intent_file_handler("show-osm.intent")
-    def handle_display_home(self):
+    def handle_display_home(self, message):
+        self.update_display_model()
         self.gui.show_page("AppstoreHome.qml", override_idle=True)
 
     @intent_file_handler("search-osm.intent")
     def handle_search_osm_intent(self, message):
         utterance = message.data.get("description", "")
+        skills = []
         if utterance is not None:
             results = self.osm_manager.search_skills(utterance)
             for s in results:
-                if s.skill_name is not None:
-                    self.search_skills_model.append({
-                        "title": s.skill_name,
-                        "description": s.skill_description,
-                        "logo": s.json.get("logo"),
-                        "author": s.skill_author,
-                        "category": s.skill_category,
-                        "url": s.url
-                        })
+                skills.append({
+                    "title": s.skill_name or s.uuid,
+                    "description": s.skill_short_description,
+                    "logo": s.json.get(
+                        "logo") or s.skill_icon or self.default_icon,
+                    "author": s.skill_author,
+                    "category": s.skill_category,
+                    "url": s.url
+                })
+            self.gui["appstore_pling_model"] = skills
 
-            self.gui["appstore_pling_model"] = self.search_skills_model
-
-    def build_skills_model(self, appstore):
-        self.log.info("Bulding model for " + appstore)
-
-        if appstore == "ovos":
-            self.log.info("Selected OVOS Appstore")
-            appstore_ovos_model = self.build_ovos_skills_model()
-            self.ovos_storage["model"] = appstore_ovos_model
-            self.ovos_storage.store()
-        elif appstore == "pling":
-            self.log.info("Selected Pling Appstore")
-            if "model" not in self.pling_storage:
-                appstore_pling_model = self.build_pling_skills_model()
-                self.pling_storage["model"] = appstore_pling_model
-                self.pling_storage.store()
-
-        else:
-            self.log.info("no valid appstore requested")
-
+    # TODO these should be unified into a single method independent of appstore
+    # for store in self.osm.appstores .....
     # Build Custom Display Model For OVOS Skill Store
-    def build_ovos_skills_model(self):
-        self.ovos_skills_model.clear()
-        for s in get_ovos_skills(parse_github=False):
-            if s.skill_name is not None:
-                self.log.info(validators.url(s.skill_icon))
-                if validators.url(s.skill_icon):
-                    skill_icon = s.skill_icon
-                else:
-                    skill_icon = "https://iconarchive.com/download/i103156\
-                    /blackvariant/button-ui-requests-9/Parcel.ico"
-                self.ovos_skills_model.append({
-                    "title": s.skill_name,
-                    "description": s.skill_description,
-                    "logo": skill_icon,
-                    "author": s.skill_author,
-                    "category": s.skill_category,
-                    "url": s.url
-                    })
+    def build_store_model(self, store_id):
+        store = self.osm_manager.get_appstore(store_id)
+        skills_model = []
+        for skill in store:
+            skill_icon = skill.skill_icon or self.default_icon
+            if not validators.url(skill.skill_icon):
+                # TODO osm should transform the relative paths!
+                # discard bad paths for now
+                skill_icon = self.default_icon
 
-        return self.ovos_skills_model
+            author, repo = author_repo_from_github_url(skill.url)
+            desc = skill.skill_short_description or \
+                   skill.skill_description or \
+                   f"{repo} by {author}"
+            skills_model.append({
+                "title": skill.skill_name or repo,
+                "description": desc,
+                "logo": skill.json.get("logo") or skill_icon,
+                "author": skill.skill_author,
+                "category": skill.skill_category,
+                "url": skill.url
+            })
 
-    # Build Custom Display Model For Pling Skill Store
-    def build_pling_skills_model(self):
-        self.pling_skills_model.clear()
-        for s in get_pling_skills(parse_github=False):
-            if s.skill_name is not None:
-                self.pling_skills_model.append({
-                    "title": s.skill_name,
-                    "description": s.skill_description,
-                    "logo": s.json.get("logo"),
-                    "author": s.skill_author,
-                    "category": s.skill_category,
-                    "url": s.url
-                    })
-
-        return self.pling_skills_model
+        return skills_model
 
     def handle_install(self, message):
         skill_url = message.data.get("url")
-        self.gui["installer_status"] = 1 # Running
+        self.gui["installer_status"] = 1  # Running
         self.log.info("Got request to install: " + skill_url)
-        self.osm_manager.install_skill_from_url(skill_url)
+        try:
+            self.osm_manager.install_skill_from_url(skill_url)
+        except Exception as e:
+            self.log.exception(e)
 
-    def sync_osm_model(self):
+    def sync_osm_model(self, message):
         self.osm_manager.sync_appstores()
 
     def update_display_model(self):
-        self.build_skills_model("ovos")
-        self.build_skills_model("pling")
-        self.update_display_data()
+        self.gui["installer_status"] = 0  # Idle / Unknown
+        self.gui["appstore_ovos_model"] = self.build_store_model("ovos")
+        self.gui["appstore_pling_model"] = self.build_store_model("pling")
 
-    def update_display_data(self):
-        self.gui["installer_status"] = 0 # Idle / Unknown
-        self.gui["appstore_ovos_model"] = self.ovos_storage["model"]
-        self.gui["appstore_pling_model"] = self.pling_storage["model"]
-
-    def display_installer_success(self):
+    def display_installer_success(self, message):
         self.log.info("Installer Successful")
-        self.gui["installer_status"] = 2 # Success
+        self.gui["installer_status"] = 2  # Success
         time.sleep(2)
-        update_display_model()
+        self.gui["installer_status"] = 0  # Idle / Unknown
 
-    def display_installer_failure(self):
+    def display_installer_failure(self, message):
         self.log.info("Installer Failed")
-        self.gui["installer_status"] = 3 # Fail
+        self.gui["installer_status"] = 3  # Fail
         time.sleep(2)
-        update_display_model()
+        self.gui["installer_status"] = 0  # Idle / Unknown
+
 
 def create_skill():
     return OSMInstaller()
